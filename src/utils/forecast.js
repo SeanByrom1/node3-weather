b0a1aa86b55785d112e46b48fd5215e80927e55c
Sean Byrom <byromsean@yahoo.com>
2019-08-05 14:48:30 -0400

    Adding in high data feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, lng, callback) => {
 		} else if (body.error) {
 			callback('Unable to find location')
 		} else {
-			callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. ')
+			callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. ' + 'The high is ' + body.daily.data[0].temperatureHigh) 
 		}
 	})
 }
